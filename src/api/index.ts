6a1dd8b0f03ac75915f9377b68d47a04ee97929c
Vladislav Kulyakin <kulyakinv@gmail.com>
2022-12-26 18:51:50 +0300

    test 1

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-export const API_URL = 'https://8b74-82-179-72-69.eu.ngrok.io/api/v1';
+export const API_URL = 'https://a280-82-179-72-69.eu.ngrok.io/api/v1';
 
 const $api = axios.create({
   withCredentials: true,
