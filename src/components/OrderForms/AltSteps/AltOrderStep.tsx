7093b8195e3b0983dec799364be03353b251eaf0
Vladislav Kulyakin <kulyakinv@gmail.com>
2023-01-11 17:46:22 +0300

    test map2

--- src/components/OrderForms/AltSteps/AltOrderStep.tsx
+++ src/components/OrderForms/AltSteps/AltOrderStep.tsx
@@ -23,6 +23,7 @@ useEffect(() => {
 onLoad(ymaps)
 ymaps.ready(init);
     function init(){
+      var myPlacemark: any
         // Создание карты.
         var myMap = new ymaps.Map("map", {
             // Координаты центра карты.
@@ -34,7 +35,56 @@ ymaps.ready(init);
             // от 0 (весь мир) до 19.
             zoom: 7
         });
+        // Слушаем клик на карте.
+    myMap.events.add('click', function (e: any) {
+      var coords = e.get('coords');
+
+      // Если метка уже создана – просто передвигаем ее.
+      if (myPlacemark) {
+          myPlacemark.geometry.setCoordinates(coords);
+      }
+      // Если нет – создаем.
+      else {
+          myPlacemark = createPlacemark(coords);
+          myMap.geoObjects.add(myPlacemark);
+          // Слушаем событие окончания перетаскивания на метке.
+          myPlacemark.events.add('dragend', function () {
+              getAddress(myPlacemark.geometry.getCoordinates());
+          });
+      }
+      getAddress(coords);
+  });
+
+  // Создание метки.
+  function createPlacemark(coords: any) {
+      return new ymaps.Placemark(coords, {
+          iconCaption: 'поиск...'
+      }, {
+          preset: 'islands#violetDotIconWithCaption',
+          draggable: true
+      });
+  }
+      // Определяем адрес по координатам (обратное геокодирование).
+      function getAddress(coords: any) {
+        myPlacemark.properties.set('iconCaption', 'поиск...');
+        ymaps.geocode(coords).then(function (res: any) {
+            var firstGeoObject = res.geoObjects.get(0);
+
+            myPlacemark.properties
+                .set({
+                    // Формируем строку с данными об объекте.
+                    iconCaption: [
+                        // Название населенного пункта или вышестоящее административно-территориальное образование.
+                        firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
+                        // Получаем путь до топонима, если метод вернул null, запрашиваем наименование здания.
+                        firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
+                    ].filter(Boolean).join(', '),
+                    // В качестве контента балуна задаем строку с адресом объекта.
+                    balloonContent: firstGeoObject.getAddressLine()
+                });
+        });
     }
+}
 
 }, [])
 
