99897dc5a617ab5fc320210fcba7561d7a751d03
Kulyakin Vladislav <kulyakinv@gmail.com>
2023-01-20 17:36:02 +0300

    alert5

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -72,11 +72,6 @@ const OrderPage: React.FC<OrderPageProps> = () => {
     setActiveStep((prevActiveStep) => (prevActiveStep !== steps.length - 1 ? prevActiveStep + 1 : prevActiveStep));
     sentPost();
   };
-  const handleNextAlert = () => {
-    setActiveStep((prevActiveStep) => (prevActiveStep !== steps.length - 1 ? prevActiveStep + 1 : prevActiveStep));
-    sentPost();
-    tg.showAlert(message)
-  };
   const [data, setData] = React.useState({});
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
@@ -85,6 +80,7 @@ const OrderPage: React.FC<OrderPageProps> = () => {
   const sentPost = () => {
     if (activeStep === 2) {
       dispatch(createTransfer(data, userId));
+      tg.showAlert(message)
     }
   };
 
@@ -114,7 +110,7 @@ const OrderPage: React.FC<OrderPageProps> = () => {
         ) : activeStep === 3 ? (
           <></>
         ) : (
-          <Button disabled={!isValid} sx={defaultButton} type="submit" onClick={handleNextAlert}>
+          <Button disabled={!isValid} sx={defaultButton} type="submit" onClick={handleNext}>
             Далее
           </Button>
         )}
