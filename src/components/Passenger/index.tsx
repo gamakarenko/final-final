72979757d94b752b6a417d45600f36680d4beedd
Vladislav Kulyakin <kulyakinv@gmail.com>
2023-01-24 19:00:22 +0300

    add order dropdown

--- src/components/Passenger/index.tsx
+++ src/components/Passenger/index.tsx
@@ -1,5 +1,8 @@
+import { Accordion, AccordionDetails, AccordionSummary } from '@mui/material';
 import { useFormContext } from 'react-hook-form';
 import PassengerBlock from './PassangerBlock';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 
 export interface PassengersProps {}
 
@@ -19,30 +22,42 @@ export const Passenger: React.FunctionComponent<PassengersProps> = () => {
       <PassengerBlock title="Куда привести?" description={order?.end} />
       <PassengerBlock title="Тип автомобиля" description={order?.carType} />
       <br />
+      <div style={{marginBottom: '15px'}}>
       {passengers &&
         passengers.map((i: any, index: any) => {
           return (
-            <div key={index}>
-              <hr />
-              <br />
-              <h3 className="passss">{`Пассажир ${index + 1}`}</h3>
-              <PassengerBlock title="ФИО" description={i?.fullName ? i?.fullName : '-'} />
-              <PassengerBlock title="Номер загранпаспорта" description={i?.passportId ? i?.passportId : '-'} />
-              <PassengerBlock
-                title="Дата и время прилёта"
-                description={i?.departureDate && i?.departureTime ? i?.departureDate + ' ' + i?.departureTime : '-'}
-              />
-              <PassengerBlock title="Номер рейса" description={'-'} />
-              <PassengerBlock title="Номер телефона" description={i?.phoneNumber ? i?.phoneNumber : '-'} />
-              <PassengerBlock title="Электронная почта" description="-" />
-              <PassengerBlock title="Логин в телеграмме" description={i?.telegramId ? i?.telegramId : '-'} />
-              <PassengerBlock
-                title="Комментарий к поездке"
-                description={i?.transferComment ? i?.transferComment : '-'}
-              />
-            </div>
+            <Accordion key={i.id}>
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1a-content"
+                      id="panel1a-header"
+                    >
+                      <h3 className="passss">{`Пассажир ${index + 1}`}</h3>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                    <div>
+                      <hr />
+                      <br />
+                      <PassengerBlock title="ФИО" description={i?.fullName ? i?.fullName : '-'} />
+                      <PassengerBlock title="Номер загранпаспорта" description={i?.passportId ? i?.passportId : '-'} />
+                      <PassengerBlock
+                        title="Дата и время прилёта"
+                        description={i?.departureDate && i?.departureTime ? i?.departureDate + ' ' + i?.departureTime : '-'}
+                      />
+                      <PassengerBlock title="Номер рейса" description={'-'} />
+                      <PassengerBlock title="Номер телефона" description={i?.phoneNumber ? i?.phoneNumber : '-'} />
+                      <PassengerBlock title="Электронная почта" description="-" />
+                      <PassengerBlock title="Логин в телеграмме" description={i?.telegramId ? i?.telegramId : '-'} />
+                      <PassengerBlock
+                        title="Комментарий к поездке"
+                        description={i?.transferComment ? i?.transferComment : '-'}
+                      />
+                    </div>
+                    </AccordionDetails>
+                  </Accordion>
           );
         })}
+        </div>
     </div>
   );
 };
