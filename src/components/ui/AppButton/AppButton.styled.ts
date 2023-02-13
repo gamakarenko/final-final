83d7954d78d37531390e5ec089fb52431dab5aee
Artyem Savchenko <a.savchenko89@yandex.ru>
2023-02-13 19:53:04 +0300

    refactor: Изменил стили дизейблед кнопок

--- src/components/ui/AppButton/AppButton.styled.ts
+++ src/components/ui/AppButton/AppButton.styled.ts
@@ -1,21 +1,41 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 import { IAppButton } from './AppButton';
 
-export const StyledAppButton = styled.button<Pick<IAppButton, 'isFilled' | 'isUppercase'>>`
+export const StyledAppButton = styled.button<
+  Pick<IAppButton, 'isFilled' | 'isUppercase'>
+>`
   padding: 11px 22px;
   border-radius: 5px;
   border: 1px var(--primary-color) solid;
-  background-color: ${(props) => (props.isFilled ? 'var(--primary-color)' : 'transparent')};
-  color: ${(props) => (props.isFilled ? '#fff' : 'var(--primary-color)')};
   text-transform: ${(props) => (props.isUppercase ? 'uppercase' : 'none')};
   line-height: 1;
   cursor: pointer;
 
   &:disabled {
-    background-color: var(--disabled-color);
-    border: 1px var(--disabled-color) solid;
-    color: #fff;
     pointer-events: none;
   }
+
+  ${({ isFilled }) =>
+    isFilled
+      ? css`
+          background-color: var(--primary-color);
+          color: #fff;
+
+          &:disabled {
+            background-color: var(--disabled-color);
+            border: 1px var(--disabled-color) solid;
+            color: #fff;
+          }
+        `
+      : css`
+          background-color: transparent;
+          color: var(--primary-color);
+
+          &:disabled {
+            border: 1px var(--disabled-color) solid;
+            color: var(--disabled-color);
+          }
+        `}
 `;
