6a1282622a841d098f011642d9da19b818047b61
Artyem Savchenko <a.savchenko89@yandex.ru>
2023-02-16 10:25:30 +0300

    Revert "refactor: Перенес роутинг в отдельный объект router"
    This reverts commit 6532d0c75921b4b1ffbdc511fba7365e840e1118.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
 
-import { router } from 'routes/routes';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+
+import App from './App';
 
 import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <App />
   </Provider>,
 );
