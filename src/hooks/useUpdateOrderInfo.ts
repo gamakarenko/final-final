a39a31992b7ffd0f1c07cfb60426e8caee8be20c
imalofeev6 <imalofeev6@gmail.com>
2023-03-10 14:19:39 +0300

    add custop hook useUpdateOrderInfo

---
+++ src/hooks/useUpdateOrderInfo.ts
@@ -0,0 +1,18 @@
+import { editOrderInfo } from "store/newOrder/newOrder";
+import { useAppDispatch } from "store/store";
+import { IOrder } from "types/order";
+
+
+
+export default function useUpdateOrderInfo(){
+
+    const dispatcher = useAppDispatch();
+    
+    const updateOrderInfo = (key: keyof IOrder, value: string) => {
+        dispatcher(editOrderInfo({[key]: value}))
+    }
+
+    return{
+        updateOrderInfo
+    }
+}
